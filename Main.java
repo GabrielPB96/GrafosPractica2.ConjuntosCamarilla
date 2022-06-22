2d26490d0d5dc98b19aa707a75a2538e457a40af
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2022-06-22 08:33:55 -0400

    Pruebas

--- Main.java
+++ Main.java
@@ -1,22 +1,24 @@
 public class Main {
   public static void main(String[] args) {
     Grafo g = new Grafo();
-    int[] verts = {0,1,2,3,4,5};
+    int[] verts = {0,1,2,3,4,5,6};
     for(Integer v : verts) {
       g.insertarVertice(v);
     }
 
-    g.insertarArista(0,1);
-    g.insertarArista(0,2);
-    g.insertarArista(0,3);
-    g.insertarArista(3,5);
-    g.insertarArista(1,5);
-    g.insertarArista(1,2);
-    g.insertarArista(2,3);
-    g.insertarArista(2,4);
-    g.insertarArista(2,5);
-    g.insertarArista(3,4);
-    g.insertarArista(5,4);
+    g.insertarArista(0, 4);
+    g.insertarArista(0, 1);
+    g.insertarArista(0, 5);
+    g.insertarArista(1, 2);
+    g.insertarArista(1, 3);
+    g.insertarArista(1, 4);
+    g.insertarArista(1, 5);
+    g.insertarArista(2, 3);
+    g.insertarArista(2, 5);
+    g.insertarArista(3, 4);
+    g.insertarArista(3, 5);
+    g.insertarArista(3, 6);
+    g.insertarArista(4, 6);
 
     System.out.println("Conjuntos Camarilla");
     System.out.println(g.conjuntosCamarilla());
